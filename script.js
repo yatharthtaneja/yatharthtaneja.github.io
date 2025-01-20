2a04d223911f3bbc706488f9e61e00fd74b5bb24
Yatharth Taneja <yt@Yatharths-MacBook-Pro.local>
2025-01-20 22:45:37 +0530

    new website check

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(move);
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX - 100;
+        tgY = event.clientY - 250;
+    });
+
+    move();
+});
+
+console.log("JavaScript file is linked correctly.");
+
